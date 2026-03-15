b2d474d0d8cf4228f667b4b754b614a9e4c86d23
Joel Felix Rubio <l22170641@culiacan.tecnm.mx>
2026-03-15 12:30:02 -0700

    se agregaron 2 clases

--- frontend/src/modulos/principal/vistas/menuVue.vue
+++ frontend/src/modulos/principal/vistas/menuVue.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-info">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">CLARUS</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,10 +9,10 @@
             <div class="navbar-nav">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav_item margin">
-                        <router-link class="nav-link item" to="/recetas">Recetas</router-link>
+                        <router-link class="nav-link item" to="/citas">Citas</router-link>
                     </li>
                     <li class="nav_item margin">
-                        <router-link class="nav-link item" to="/citas">Citas</router-link>
+                        <router-link class="nav-link item" to="/recetas">Recetas</router-link>
                     </li>
                     <li class="nav_item margin">
                         <router-link class="nav-link item" to="/perfil">Perfil</router-link>
