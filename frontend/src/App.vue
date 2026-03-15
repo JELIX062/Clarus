94bac078630838e0d76f042ca1e56efe0fbf9095
Joel Felix Rubio <l22170641@culiacan.tecnm.mx>
2026-03-14 19:18:03 -0700

    Proyecto final godoy

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import menuVue from './modulos/principal/vistas/menuVue.vue';
+
+
+</script>
+
+<template>
+  <header>
+    <menuVue/>
+  </header>
+</template>
+
+<style scoped></style>
---
+++ frontend/src/modulos/principal/vistas/menuVue.vue
@@ -0,0 +1,33 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-info">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">APLICACIÓN</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav_item margin">
+                        <router-link class="nav-link item" to="/recetas">Recetas</router-link>
+                    </li>
+                    <li class="nav_item margin">
+                        <router-link class="nav-link item" to="/citas">Citas</router-link>
+                    </li>
+                    <li class="nav_item margin">
+                        <router-link class="nav-link item" to="/perfil">Perfil</router-link>
+                    </li>
+                </ul>
+            </div>
+            </div>
+        </div>
+</nav>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
